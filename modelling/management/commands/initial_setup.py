6bb69718ce16611baddcf657a89917e44af56cce
AppDeveloper <appdeveloper@example.com>
2023-06-02 23:25:19 +0300

    remove redundant dir creation code

--- modelling/management/commands/initial_setup.py
+++ modelling/management/commands/initial_setup.py
@@ -17,32 +17,5 @@ from modelling.data_preparation.manager import MITDBDatasetManager
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        self.__create_dirs()
-
         manager = MITDBDatasetManager()
         manager.cache_features()
-
-    def __create_dirs(self):
-        if not os.path.isdir(DATA_PREPARATION_CACHE_DIR):
-            os.mkdir(DATA_PREPARATION_CACHE_DIR)
-
-        if not os.path.isdir(MITDB_DATASET_DIR):
-            os.makedirs(MITDB_DATASET_DIR)
-
-        if not os.path.isdir(MITDB_FEATURES_DIR):
-            os.makedirs(MITDB_FEATURES_DIR)
-
-        if not os.path.isdir(MODELS_CACHE_DIR):
-            os.mkdir(MODELS_CACHE_DIR)
-
-        if not os.path.isdir(SCALER_MODELS_CACHE_DIR):
-            os.mkdir(SCALER_MODELS_CACHE_DIR)
-
-        if not os.path.isdir(KMEANS_MODELS_CACHE_DIR):
-            os.mkdir(KMEANS_MODELS_CACHE_DIR)
-
-        if not os.path.isdir(WORD2VEC_MODELS_CACHE_DIR):
-            os.mkdir(WORD2VEC_MODELS_CACHE_DIR)
-
-        if not os.path.isdir(CLASSIFIER_MODELS_CACHE_DIR):
-            os.mkdir(CLASSIFIER_MODELS_CACHE_DIR)
